2058f9f4c1549fb1f6cd6d4ba533b53a3d99c1ac
spartin07 <n.orourke98@gmail.com>
2024-04-16 15:06:24 -0700

    Add Locations, location display and set starting location to home.

--- Engine/ViewModels/GameSession.cs
+++ Engine/ViewModels/GameSession.cs
@@ -4,12 +4,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace Engine.ViewModels
 {
    public class GameSession
     {
         public Player CurrentPlayer { get; set; }
+
+        public Location CurrentLocation { get; set; }
         public GameSession()
         {
             CurrentPlayer = new Player
@@ -20,6 +23,15 @@ namespace Engine.ViewModels
                 HitPoints = 10,
                 ExperiencePoints = 0,
                 Level = 1
+            };
+
+            CurrentLocation = new Location
+            {
+                Name = "Home",
+                XCoordinate = 0,
+                YCoordinate = -1,
+                Description = "This is your home",
+                ImageName = "\\Engine;component\\Images\\Locations\\Home.png"
             };
         }
     }
