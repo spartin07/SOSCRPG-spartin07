2ae230c52e1182e4662188743c4257f5185a8467
spartin07 <n.orourke98@gmail.com>
2024-04-16 14:11:42 -0700

    UI displays current player OnPropertyChanged events added Can add xp through button

---
+++ Engine/ViewModels/GameSession.cs
@@ -0,0 +1,26 @@
+﻿using Engine.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine.ViewModels
+{
+   public class GameSession
+    {
+        public Player CurrentPlayer { get; set; }
+        public GameSession()
+        {
+            CurrentPlayer = new Player
+            {
+                Name = "Niall",
+                Gold = 100000,
+                CharacterClass = "Fighter",
+                HitPoints = 10,
+                ExperiencePoints = 0,
+                Level = 1
+            };
+        }
+    }
+}
